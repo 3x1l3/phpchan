a63b07df9c2f834ef0cd70a3fd2816ca14346982
Chad Klassen <chadklassen1@gmail.com>
2016-03-09 16:44:46 -0700

    Some more changes

--- thread.php
+++ thread.php
@@ -16,22 +16,22 @@ $thread = json_decode($controller->get($url));
 
 $count = 0;
 
-$gif = array();
-$webm = array();
+$gif = new Content();
+$webm = new Content();
 
 foreach ($thread->posts as $post) {
 	if ($post->filename) {
 		if ($post->ext != '.webm') {
-			$gif[] = '<div class="thumb-cell well well-sm">';
-			$gif[] = '<a class="image" href="' . $controller->genImageUrl($post) . '"><img class="thumb" src="' . $controller->genThumnailURL($post->tim) . '" /></a>';
-			$gif[] =  '</div>';
+			$gif->Add('<div class="thumb-cell well well-sm">');
+			$gif->Add('<a class="image" href="' . $controller->genImageUrl($post) . '"><img class="thumb" src="' . $controller->genThumnailURL($post->tim) . '" /></a>');
+			$gif->Add('</div>');
 		} else {
 			$id = md5($post->filename);
 			
-			$webm[] =  '<div class="thumb-cell well well-sm">';
-			$webm[] = '<a class="webm"  href="#' .$id. '"><img class="thumb" src="' . $controller->genThumnailURL($post->tim) . '" /></a>';
-			$webm[] =  '<div style="display: none"><div id="'.$id.'"><video src="' . $controller->genImageUrl($post) . '" controls></video></div></div>';
-			$webm[] =  '</div>';
+			$webm->Add('<div class="thumb-cell well well-sm">');
+			$webm->Add('<a class="webm"  href="#' .$id. '"><img class="thumb" src="' . $controller->genThumnailURL($post->tim) . '" /></a>');
+			$webm->Add('<div style="display: none"><div id="'.$id.'"><video src="' . $controller->genImageUrl($post) . '" controls></video></div></div>');
+			$webm->Add('</div>');
 		}
 
 		$count++;
@@ -40,9 +40,9 @@ foreach ($thread->posts as $post) {
 }
 echo '<div class="gallery">';
 echo '<h3>WebM</h3>';
-echo implode('',$webm);
+echo '<div>'.$webm.'</div>';
 echo '<h3>Other</h3>';
-echo implode('', $gif);
+echo '<div>'.$gif;
 echo '</div>';
 
 echo $view->footer();
