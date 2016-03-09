b1c2794995a2f5e0478312feb14dcbe958ac6405
Chad Klassen <chadklassen1@gmail.com>
2016-03-09 16:15:52 -0700

    Changed back to 5 column width on thread view. Added title.

--- view.php
+++ view.php
@@ -10,7 +10,7 @@ class View {
 		
 		$content = new Content();
 		$content->add( '<!DOCTYPE html>');
-		$content->add( '<html><head>');
+		$content->add( '<html><head><title>PHPChan</title>');
 		$content->add( '<meta name="viewport" content="width=device-width,initial-scale=1.0">');
 		$content->add( '<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />');
 		$content->add( '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />');
