9aba6500bebd905b97fdddca3c16ab9e3c013a8e
Chad Klassen <chadklassen1@gmail.com>
2016-03-13 23:09:22 -0600

    Don't need to use curl, but will default to it if it exists.

--- controller.php
+++ controller.php
@@ -1,92 +1,101 @@
 <?php
 
-class Controller {
+class Controller
+{
+    private $board;
+    private $page;
 
-	private $board;
-	private $page;
-	
-	private $hash;
+    private $hash;
 
-	private $thumbnail_endpoint = 'http://t.4cdn.org/[board]/[tim]s.jpg ';
-	private $image_endpoint = 'http://i.4cdn.org/[board]/[tim][ext]';
+    private $thumbnail_endpoint = 'http://t.4cdn.org/[board]/[tim]s.jpg ';
+    private $image_endpoint = 'http://i.4cdn.org/[board]/[tim][ext]';
 
-	public function __construct() {
-		$this->board = $_GET['b'];
-		$this->page = $_GET['p'];
+    public function __construct()
+    {
+        $this->board = $_GET['b'];
+        $this->page = $_GET['p'];
 
-		if (!$this->page)
-			$this->page = 1;
-		
-		$this->hash = md5($this->board.$this->page);
+        if (!$this->page) {
+            $this->page = 1;
+        }
 
-	}
+        $this->hash = md5($this->board.$this->page);
+    }
 
-	public function get($url) {
+    public function get($url)
+    {
 
-		$cache = new phpFastCache();
+        // $cache = new phpFastCache();
+        //
+        // $response = $cache->get($url);
+        // $response = null;
 
-		$response = $cache->get($url);
-		$response = null;
-		if ($response == null) {
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($ch);
-			curl_close($ch);
-			$cache->set($url,$response, 600);
-		}
+        if ($this->curlEnabled()) {
+            if ($response == null) {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($ch);
+                curl_close($ch);
+                $cache->set($url, $response, 600);
+            }
+        } else {
+            $response = file_get_contents($url);
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 
-	private function buildURL() {
+    public function curlEnabled()
+    {
+        return function_exists('curl_version');
+    }
 
-		return 'http://a.4cdn.org/' . $this->board . '/' . $this->page . '.json';
+    private function buildURL()
+    {
+        return 'http://a.4cdn.org/'.$this->board.'/'.$this->page.'.json';
+    }
 
-	}
+    public function exec()
+    {
+        return $this->get($this->buildURL());
+    }
 
-	public function exec() {
+    public function genThumnailURL($tim)
+    {
+        $tmp = $this->thumbnail_endpoint;
+        $tmp = str_replace('[board]', $this->board, $tmp);
+        $tmp = str_replace('[tim]', $tim, $tmp);
 
-		return $this->get($this->buildURL());
+        return $tmp;
+    }
 
-	}
+    public function genImageUrl($post)
+    {
+        $tmp = $this->image_endpoint;
+        $tmp = str_replace('[board]', $this->board, $tmp);
+        $tmp = str_replace('[tim]', $post->tim, $tmp);
+        $tmp = str_replace('[ext]', $post->ext, $tmp);
 
-	public function genThumnailURL($tim) {
+        return $tmp;
+    }
 
-		$tmp = $this->thumbnail_endpoint;
-		$tmp = str_replace('[board]', $this->board, $tmp);
-		$tmp = str_replace('[tim]', $tim, $tmp);
-		return $tmp;
+    public function getThumbnails($json)
+    {
+        $thumbs = array();
+        $obj = json_decode($json);
+        if (isset($obj->threads)) {
+            foreach ($obj->threads as $thread) {
+                if (isset($thread->posts)) {
+                    foreach ($thread->posts as $post) {
+                        if (isset($post->tim) && isset($post->replies)) {
+                            $thumbs[] = $this->genThumnailURL($post->tim);
+                        }
+                    }
+                }
+            }
+        }
 
-	}
-
-	public function genImageUrl($post) {
-
-		$tmp = $this->image_endpoint;
-		$tmp = str_replace('[board]', $this->board, $tmp);
-		$tmp = str_replace('[tim]', $post->tim, $tmp);
-		$tmp = str_replace('[ext]', $post->ext, $tmp);
-		return $tmp;
-
-	}
-
-	public function getThumbnails($json) {
-		$thumbs = array();
-		$obj = json_decode($json);
-		if (isset($obj->threads)) {
-			foreach ($obj->threads as $thread) {
-				if (isset($thread->posts)) {
-					foreach ($thread->posts as $post) {
-
-						if (isset($post->tim) && isset($post->replies)) {
-							$thumbs[] = $this->genThumnailURL($post->tim);
-						}
-
-					}
-				}
-			}
-		}
-		return $thumbs;
-	}
-
+        return $thumbs;
+    }
 }
--- index.php
+++ index.php
@@ -1,25 +1,20 @@
-<?php     
-require_once("config.php");
+<?php
 
-
+require_once 'config.php';
 
 $controller = new Controller();
 $view = new View();
 
-
-
-$array = json_decode($controller -> get("http://a.4cdn.org/boards.json"));
+$array = json_decode($controller->get('http://a.4cdn.org/boards.json'));
 
 $boards = new Content();
 
 $boards->add('<ul class="list-group">');
 foreach ($array->boards as $board) {
-	$boards->add('<li class="list-group-item"><a href="board.php?b='.$board->board.'">'.$board->title.'</a></li>');
+    $boards->add('<li class="list-group-item"><a href="board.php?b='.$board->board.'">'.$board->title.'</a></li>');
 }
 
 $boards->add('</ul>');
 
-
-echo $view -> header();
+echo $view->header();
 echo $boards;
-
