b5de5fc14f210d836ba905b02dacb6ca3a77a1d7
Chad <chadklassen1@gmail.com>
2016-04-12 21:39:07 -0600

    Gave up on the video load progress bar. Remove video tag once modal is hidden.

--- js/site.js
+++ js/site.js
@@ -1,66 +1,57 @@
 $(document).ready(function() {
 
 
-//	$('a.image').fancybox();
+
+    $('a.popup-trigger').click(
+        function() {
+
+            var href = $(this).data('img');
+            var width = $(this).data('width');
+            var type = $(this).data('type');
 
 
-	$('a.popup-trigger').click(
-		function() {
+            if (type == 'image') {
+                $('#popup i.fullscreen-icon').show();
 
-		var href = $(this).data('img');
-		var width = $(this).data('width');
-		var type = $(this).data('type');
+                if (width > $('#popup .modal-dialog').width()) {
+                    var img = $('<a href="' + href + '" target="_blank"><img class="fade" src="' + href + '" /></a>');
+                    img.find('img').on('load', function() {
+                        $('#popup .modal-body').html(img);
+                        $('#popup').modal('show');
+                    });
+                } else {
 
+                    var img = $('<img src="' + href + '" />');
+                    img.on('load', function() {
+                        $('#popup .modal-body').html(img);
+                        $('#popup').modal('show');
+                    });
 
-		if (type == 'image') {
-			$('#popup i.fullscreen-icon').show();
+                }
+            } else {
+                $('#popup i.fullscreen-icon').hide();
 
-		if (width > $('#popup .modal-dialog').width()) {
-			var img = $('<a href="'+href+'" target="_blank"><img class="fade" src="'+ href  +'" /></a>');
-			img.find('img').on('load', function() {
-				$('#popup .modal-body').html(img);
-				$('#popup').modal('show');
-			});
-		} else {
+                var video = $('<video id="vid" src="' + href + '" controls style="display: none;"></video>' + '<i class="fa fa-spin fa-spinner loading"></i>');
 
-			var img = $('<img src="'+ href  +'" />');
-			img.on('load', function() {
-				$('#popup .modal-body').html(img);
-				$('#popup').modal('show');
-			});
+                $('#popup .modal-body').html(video);
+                $('#popup').modal('show');
+                //	$('#popup .modal-dialog').width();
+                $('#popup .modal-body video').on('canplaythrough', function() {
 
-		}
-	} else {
-$('#popup i.fullscreen-icon').hide();
+                    $(this).attr('style', '');
+                    $('#popup').find('.fa-spinner').remove();
 
-		var video = $('<video src="' + href + '" controls style="display: none;"></video>'+'<div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div>');
-
-			$('#popup .modal-body').html(video);
-			$('#popup').modal('show');
-		//	$('#popup .modal-dialog').width();
-		$('#popup .modal-body video').on('canplaythrough',function(){
-
-			$(this).attr('style','');
-			$('#popup').find('.fa-spinner').remove();
-
-		});
+                });
 
 
-$('#popup video').bind("progress",function(e){
-	console.log(e);
-	var percentComplete = e.loaded/e.total;
-        console.log(e.total + ' ' + e.loaded + ' ' + e.lengthComputable );
-					$('#popup .progress .progress-bar').width(percentComplete+'%')
+
+            }
+
+        });
+
+    $('#popup').on('hide.bs.modal', function() {
+        $('#popup video').remove();
     });
 
-
-	}
-
-
-
-
-
-
-	});
 
 });
